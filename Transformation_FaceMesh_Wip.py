a2aa55c90067d4b59d6682d98e7f5e5b24f43920
Noah Buchanan <noah6544@gmail.com>
2023-10-29 09:46:53 -0400

    Storing any changes previous to current.
    I updated heic to jpg, check that whole repository. i updated the transformation facemesh I think from the last commit, and the webcam as well. not as much as what I'm about to do, I'm so excited man.

--- Transformation_FaceMesh_Wip.py
+++ Transformation_FaceMesh_Wip.py
@@ -9,7 +9,7 @@ import random
 ###IMPORTS
 
 ###VARIABLES
-images_path = "C:\\Users\\Noah\'s Marc P. 4648\\Pictures\\DAILY PIC\\"
+images_path = "daily"
 daily_photo_path = "Daily Photo Converted\\"
 adjusted_images_path = "adjusted images\\"
 mp_face_mesh = mp.solutions.face_mesh
@@ -17,6 +17,8 @@ mp_face_detection = mp.solutions.face_detection
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 drawing_spec = mp_drawing.DrawingSpec(thickness=0, circle_radius=1)
+
+
 
 
 def scale_around_point(image, point, scale):
@@ -63,6 +65,11 @@ def rotate_image(image, angle, pivot_point):
 	result = cv.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv.INTER_LINEAR)
 	return result
 
+def scaleImagetoBaseImage(image,Xdifference,ConstantXdifference):
+	scaleFactor = ConstantXdifference - Xdifference
+	print("scale Factor: " + str(scaleFactor))
+	return cv.resize(image, (image.shape[:2]), scaleFactor*10,scaleFactor*10)
+
 
 def bruteForceRotation(image,rightEyex,rightEyey):
 	rotationAmount = 4
@@ -85,38 +92,65 @@ def bruteForceRotation(image,rightEyex,rightEyey):
 	cv.destroyAllWindows()
 
 	return rotatedImage
-			
+	
+#ToDo: implement a cool "drawing with your eyes." kinda thing
+
+def getBaseImageStats(baseimage):
+	LeftEyeImageCoordinates = getImageCoordinates(baseimage,468)
+	RightEyeImageCoordinates = getImageCoordinates(baseimage,473)
+	LeftEyex, LeftEyey = LeftEyeImageCoordinates[0],LeftEyeImageCoordinates[1]
+	RightEyex, RightEyey = RightEyeImageCoordinates[0],RightEyeImageCoordinates[1]
+	Ydifference = RightEyey - LeftEyey
+	Xdifference = RightEyex - LeftEyex
+	return ([LeftEyex,LeftEyey],[RightEyex,RightEyey],[Xdifference,Ydifference])
 
   
+
 	
 ###RUNNING CODE
 
 
 baseimage = cv.imread("baseimage.jpg")
+BaseImageStats = getBaseImageStats(baseimage)
+print(BaseImageStats)
+count = 1
 
-
+landmarkNumber = 468
 #specifically this for loop gets all files and only keeps the ones that are jpg files and aren't curropted or 0 in size.
 for file in os.listdir(daily_photo_path):
-	#endswith("g") because that's for png/jpg files. I didn't know how to check for the last 4 position slots because each file name size is different and the initial start is different. anyways this works currently
-	if file.endswith("g") and os.path.getsize(daily_photo_path + file) > 0 and file != "1871.jpg":
-		cvimage = cv.imread(daily_photo_path + file)
-		#cv2image = cv.resize(cv2image, (width,height))
-		currentImageCoordinates = getImageCoordinates(cvimage,473)
-		print(currentImageCoordinates)
-		if currentImageCoordinates != None: #if successfully found image
-			currentImagex, currentImagey = currentImageCoordinates[0],currentImageCoordinates[1]
-			initialx, initialy = cvimage.shape[0]*.55,cvimage.shape[1]*.34 #.55 and .34 are arbitary numbers i picked.
-			print(cvimage.shape)
-			movex = initialx - currentImagex 
-			movey = initialy - currentImagey  
-			finalimage = scale_around_point(cvimage,(currentImagex,currentImagey),-currentImageCoordinates[2])
-			print(movex,movey)
-			finalimage = translate(cvimage,movex,movey)
-			finalimage = bruteForceRotation(finalimage,currentImagex,currentImagey)
-			cv.imshow("fingerscrossed",finalimage)
-			cv.waitKey(10)
-			cv.imwrite(adjusted_images_path+file+"_adjusted.jpg",finalimage)
-		else: 
-			print("No face was found for file: " + file)
+	if file in os.listdir(adjusted_images_path):
+		pass
 	else:
-		pass	
+		#endswith("g") because that's for png/jpg files. I didn't know how to check for the last 4 position slots because each file name size is different and the initial start is different. anyways this works currently
+		if file.endswith("g") and os.path.getsize(daily_photo_path + file) > 0 and file != "1871.jpg":
+			cvimage = cv.imread(daily_photo_path + file)
+			#cv2image = cv.resize(cv2image, (width,height))
+			LeftEyeImageCoordinates = getImageCoordinates(cvimage,468)
+			RightEyeImageCoordinates = getImageCoordinates(cvimage,473)
+			if RightEyeImageCoordinates and LeftEyeImageCoordinates != None: #if successfully found image
+				LeftEyex, LeftEyey = LeftEyeImageCoordinates[0],LeftEyeImageCoordinates[1]
+				RightEyex, RightEyey = RightEyeImageCoordinates[0],RightEyeImageCoordinates[1]
+
+				if landmarkNumber == 468: #if we want the left eye
+					initialx, initialy = BaseImageStats[0][0],BaseImageStats[0][1] #.55 and .34 are arbitary numbers i picked.
+					movex = initialx - LeftEyex 
+					movey = initialy - LeftEyey
+					# image = scaleImagetoBaseImage(cvimage,(RightEyex-LeftEyex),BaseImageStats[2][0])
+					finalimage = translate(cvimage,movex,movey)
+					print(str(movex) + "move x")
+				elif landmarkNumber == 473: #if we want the right eye
+					initialx, initialy = BaseImageStats[1][0],BaseImageStats[1][1] #.55 and .34 are arbitary numbers i picked.
+					movex = initialx - RightEyex 
+					movey = initialy - Righteyey  
+					finalimage = translate(cvimage,movex,movey)
+
+				# cv.imshow("Newly Aligned Image",finalimage)
+				# cv.waitKey(1)
+				cv.imwrite(adjusted_images_path+file+"_adjusted.jpg",finalimage)
+				print("Successfully aligned and wrote to file image: \'" + str(file) +"\' #" + str(count))
+				count += 1
+			
+			else: 
+				print("No face was found for file: " + file)	
+		else:
+			pass
--- webcam_facemesh.py
+++ webcam_facemesh.py
@@ -7,6 +7,20 @@ mp_face_mesh = mp.solutions.face_mesh
 
 
 
+def scale_around_point(image, point, scale):
+	# Get the size of the image
+	(h, w) = image.shape[:2]
+
+	# Translate the point to the center of the image
+	center = (w // 2, h // 2)
+	M = cv2.getRotationMatrix2D(center, 0, 1.0)
+	(tx, ty) = ((center[0] - point[0]) * scale, (center[1] - point[1]) * scale)
+	M[0, 2] += tx
+	M[1, 2] += ty
+
+	# Apply the transformation
+	return cv2.warpAffine(image, M, (w, h))
+
 
 def translate(img, x, y):
 	transMat = np.float32([[1,0,x],[0,1,y]])
@@ -14,12 +28,89 @@ def translate(img, x, y):
 	return cv2.warpAffine(img, transMat, dimensions)
 
 
+def scaleImagetoBaseImage(image,Xdifference,ConstantXdifference):
+	#maybe create a loop to keep scalling it until the scale factor is close to 0.
+	distanceDifference = ConstantXdifference - Xdifference
+	scaleFactor = 1
+	print("scale Factor: " + str(scaleFactor))
+	running = True
+	while distanceDifference != 0:
+		if scaleFactor > 0: #if face is larger than baseimage: then shrink it
+			testimage = cv2.resize(image, (image.shape[0],image.shape[1]), -1*scaleFactor,-1*scaleFactor)
+		elif scaleFactor <0: #if face is smaller than baseimage: then scale it up
+			testimage = cv2.resize(image, (image.shape[0],image.shape[1]), scaleFactor,scaleFactor)
+
+		LeftEyeImageCoordinates = getImageCoordinates(testimage,468)
+		RightEyeImageCoordinates = getImageCoordinates(testimage,473)
+		LeftEyex, LeftEyey = LeftEyeImageCoordinates[0],LeftEyeImageCoordinates[1]
+		RightEyex, RightEyey = RightEyeImageCoordinates[0],RightEyeImageCoordinates[1]
+		distanceDifference = ConstantXdifference - (RightEyex-LeftEyex)
+		scaleFactor += 10
+		print("distanceDifference: " + str(distanceDifference))
+		print("Left eye coordinates: " + str(LeftEyeImageCoordinates) + "\nRight Eye coordinates: " + str(RightEyeImageCoordinates))
+		print("Test image X Value: "+ str(RightEyex) +" Y value: " + str(LeftEyex))
+		print("Scale Factor: " + str(scaleFactor))
+		cv2.imshow("growing window",testimage)
+		cv2.waitKey(10)
+		cv2.destroyAllWindows() 
+		if distanceDifference  == 0:
+			return testimage
+	
+def getBaseImageStats(baseimage):
+	print(baseimage.shape[:-1])
+	LeftEyeImageCoordinates = getImageCoordinates(baseimage,468)
+	RightEyeImageCoordinates = getImageCoordinates(baseimage,473)
+	LeftEyex, LeftEyey = LeftEyeImageCoordinates[0],LeftEyeImageCoordinates[1]
+	RightEyex, RightEyey = RightEyeImageCoordinates[0],RightEyeImageCoordinates[1]
+	Ydifference = RightEyey - LeftEyey
+	Xdifference = RightEyex - LeftEyex
+	return ([LeftEyex,LeftEyey],[RightEyex,RightEyey],[Xdifference,Ydifference])
+
+def getImageCoordinates(img,targetlandmark):
+	imgHeight, imgWidth = img.shape[:2]
+	with mp_face_mesh.FaceMesh(static_image_mode=True,max_num_faces=1,refine_landmarks=True,min_detection_confidence=0.5) as face_mesh:
+		# Convert the BGR image to RGB and process it with MediaPipe Face Detection.
+		results = face_mesh.process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+		if not results.multi_face_landmarks: #if there are no face landmarks detected it will ignore.
+			return None
+		# Print and draw face mesh landmarks on image.
+		for face_landmarks in results.multi_face_landmarks:	
+			# mp_drawing.draw_landmarks(
+			# 	image=image,
+			# 	landmark_list=face_landmarks,
+			# 	connections=mp_face_mesh.FACEMESH_TESSELATION,
+			# 	landmark_drawing_spec=None,
+			# 	connection_drawing_spec=mp_drawing_styles
+			# 	.get_default_face_mesh_tesselation_style())
+			# mp_drawing.draw_landmarks(
+			# 	image=image,
+			# 	landmark_list=face_landmarks,
+			# 	connections=mp_face_mesh.FACEMESH_CONTOURS,
+			# 	landmark_drawing_spec=None,
+			# 	connection_drawing_spec=mp_drawing_styles
+			# 	.get_default_face_mesh_contours_style())
+			# mp_drawing.draw_landmarks(
+			# image=image,
+			# landmark_list=face_landmarks,
+			# connections=mp_face_mesh.FACEMESH_IRISES,
+			# landmark_drawing_spec=None,
+			# connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_iris_connections_style())
+		
+			face_id_points = []
+			for id, landmark in enumerate(face_landmarks.landmark):
+				if id == targetlandmark: #this is the point I'm targeting: 468 is the left eye, 473 for the right looks great.
+					currentImageCoordinates = [int(landmark.x*imgWidth),int(landmark.y*imgHeight),landmark.z]
+					return currentImageCoordinates
+				else:
+					pass
+
 face_id_points = []
 # For webcam input:
 drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
+count = 0 
 with mp_face_mesh.FaceMesh(
 		max_num_faces=2,
 		refine_landmarks=True,
@@ -34,52 +125,36 @@ with mp_face_mesh.FaceMesh(
 		# To improve performance, optionally mark the image as not writeable to
 		# pass by reference.
 		image.flags.writeable = False
-		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-		results = face_mesh.process(image)
-		# Draw the face mesh annotations on the image.
-		image.flags.writeable = True
-		image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)		
-		if results.multi_face_landmarks:
-			for face_landmarks in results.multi_face_landmarks:
-				
-				# mp_drawing.draw_landmarks(
-				# 		image=image,
-				# 		landmark_list=face_landmarks,
-				# 		connections=mp_face_mesh.FACEMESH_TESSELATION,
-				# 		landmark_drawing_spec=None,
-				# 		connection_drawing_spec=mp_drawing_styles
-				# 		.get_default_face_mesh_tesselation_style())
-				# mp_drawing.draw_landmarks(
-				# 		image=image,
-				# 		landmark_list=face_landmarks,
-				# 		connections=mp_face_mesh.FACEMESH_CONTOURS,
-				# 		landmark_drawing_spec=None,
-				# 		connection_drawing_spec=mp_drawing_styles
-				# 		.get_default_face_mesh_contours_style())
-				mp_drawing.draw_landmarks(
-						image=image,
-						landmark_list=face_landmarks,
-						connections=mp_face_mesh.FACEMESH_IRISES,
-						landmark_drawing_spec=None,
-						connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_iris_connections_style())
-				for id,landmark in enumerate(face_landmarks.landmark):
-					image_height, image_width, image_c = image.shape
-					x,y= (landmark.x*image_width),(landmark.y*image_height)
-					# print(landmark.x)
-					# print(landmark.y)
-					# print("landmarky, x ^^")
-					# print(str(x)+ ": x value\ny value: " + str(y))
-					# movex = (.55*image_width) - x
-					# movey = (.7*image_height) - y
-					# image = translate(image,movex,movey)
-					# #(x*image_width, y* image_height # the normal x is like .4, but multyping x and y by width and height (Respectively), gives you where it is. don't convert to int becauses then it looses accuracy.
-					# face_id_points.append([x,y])
-					cv2.putText(image, str(id), (int(x), int(y)), cv2.FONT_HERSHEY_PLAIN, 0.6, (0,0,255), 1)
+		landmarkNumber = 468
+		LeftEyeImageCoordinates = getImageCoordinates(image,468)
+		RightEyeImageCoordinates = getImageCoordinates(image,473)
+		
 
-	
+		if LeftEyeImageCoordinates or RightEyeImageCoordinates != None: #if successfully found image ##CHECK VERSION HISTORY
+			LeftEyex, LeftEyey = LeftEyeImageCoordinates[0],LeftEyeImageCoordinates[1]
+			RightEyex, RightEyey = RightEyeImageCoordinates[0],RightEyeImageCoordinates[1]
+			if count == 0:
+				BaseImageStats = getBaseImageStats(image)
+				print("BaseImageStats: "+ str(BaseImageStats))
+				count = 1
+			else:
+				if landmarkNumber == 468: #if we want the left eye
+					initialx, initialy = BaseImageStats[0][0],BaseImageStats[0][1] #.55 and .34 are arbitary numbers i picked.
+					movex = initialx - LeftEyex 
+					movey = initialy - LeftEyey
+					image = scaleImagetoBaseImage(image,(RightEyex-LeftEyex),BaseImageStats[2][0])
+					image = translate(image,movex,movey)
+					print(str(movex) + "move x")
+				elif landmarkNumber == 473: #if we want the right eye
+					initialx, initialy = BaseImageStats[1][0],BaseImageStats[1][1] #.55 and .34 are arbitary numbers i picked.
+					movex = initialx - RightEyex 
+					movey = initialy - RightEyey  
+					image = translate(image,movex,movey)
+		else: 
+			print("No face was found for this frame: ")
 
 		# Flip the image horizontally for a selfie-view display.
-		cv2.imshow('MediaPipe Face Mesh', cv2.flip(image, 1))  #THIS FLIP, if you're trying to find landmarks, take this into consideration.
+		cv2.imshow('MediaPipe Face Mesh', image)  #THIS FLIP, if you're trying to find landmarks, take this into consideration.
 		if cv2.waitKey(5) & 0xFF == 27:
 			break
 cap.release()
